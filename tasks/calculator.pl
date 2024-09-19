085a10d182c49445ca8ff14b3cce7dc4217b68b7
Olga Nikonova <olga-nikonova-00@mail.ru>
2024-09-19 13:33:58 +0300

    add calculator

---
+++ tasks/calculator.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+# Вввод значений для математической операции
+print "Введите число действие число\n";
+my $param1;
+my $action;
+my $param2;
+while ( chomp( $_ = <STDIN> ) ) {
+    if ( /^(-?\d+) ([\+\-\/\*]{1}) (-?\d+)$/ ) {
+        $param1 = $1;
+        $action = $2;
+        $param2 = $3;
+        last;
+    } else {
+        print "Ошибка, $_ не математическим выражением, введите выражение\n";
+    };
+}
+
+# Вычисление результата выражения
+my $result;
+if ( $action eq "+" ) {
+    $result = $param1 + $param2;
+} elsif ( $action eq "-" ) {
+    $result = $param1 - $param2;
+} elsif ( $action eq "/" ) {
+    $result = $param1 / $param2;
+} else {
+    $result = $param1 * $param2;
+}
+
+# Очистка экрана
+system 'cls';
+
+# Вывод результата
+print "$param1 $action $param2 = $result";
