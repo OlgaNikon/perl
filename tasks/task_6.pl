ddb3a1c3652865b76f074ddb4a5c08d95ace2bec
Olga Nikonova <olga-nikonova-00@mail.ru>
2024-09-20 14:47:33 +0300

    add task 6

---
+++ tasks/task_6.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+# Функция прочтения файла и сохранения его в хеш
+sub _task_6_read_conf {
+
+    # Открываем файл
+    my $conf_path = "tasks/files/conf_with_errors.ini";
+    if ( ! open FILE, "<", $conf_path ) {
+        print "Can't open '$conf_path': $!";
+    }
+
+    # Если удалось открыть файл, читаем, форматируем строки и проверяем на соответствия, сохраняем в хеш
+    my %users;
+    while ( <FILE> ) {
+        $_ =~ s/ \s //xg;
+        if ( $_ =~ m/ ( ^ \#{0} \w* ) = ( \w* ) /x ) {
+            $users{$1} = $2;
+        }
+    }
+
+    # Закрытие файла
+    close FILE;
+
+    return %users;
+}
+
+# Вызов функции прочтения файла и сохранение результата в хеш
+my %users = &_task_6_read_conf;
+
+# Вывод хеша
+while ( ( my $key, my $value ) = each %users ) {
+    print "$key => $value\n";
+}
