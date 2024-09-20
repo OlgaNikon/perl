b664a01646903050434fb705fa3b31729dd31d9e
Olga Nikonova <olga-nikonova-00@mail.ru>
2024-09-20 14:47:24 +0300

    add task 5

---
+++ tasks/task_5.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+# Функция прочтения файла и сохранения его в массив
+sub _task_5_read_conf {
+
+    # Открываем файл
+    my $conf_path = "tasks/files/conf.ini";
+    if ( ! open FILE, "<", $conf_path ) {
+        print "Can't open '$conf_path': $!";
+    }
+
+    # Если удалось открыть файл, читаем и сохраняем его в массив
+    my @users;
+    while ( <FILE> ) {
+        chomp;
+        push @users, $_;
+    }
+
+    # Закрытие файла
+    close FILE;
+
+    return @users;
+}
+
+# Вызов функции прочтения файла и сохранение результата в массив
+my @users = &_task_5_read_conf;
+
+# Вывод массива
+printf "%s\n" x @users, @users;
