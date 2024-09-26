f996337e43a9710c9f4f89cdd20264839a486fb1
Olga Nikonova <olga-nikonova-00@mail.ru>
2024-09-26 09:11:55 +0300

    add task 10

---
+++ tasks/task_10.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+use lib 'C:/first/tasks/';
+use tools;
+
+# Получаем данные
+my ( $user_name, $user_password, $action ) = @ARGV;
+
+# Вызываем функию для регистрации или входа или удаления
+if ( $action eq "log" ) {
+    tools::task_8_login;
+} elsif ( $action eq "reg" ) {
+    # Вызов функции для проверки корректности логина и пароля
+    my $check_usr_name_password = tools::task_9_check_user_name( $user_name );
+    $check_usr_name_password *= tools::task_9_check_user_password( $user_password );
+
+    # Если логин и пароль прошел проверку вызываем функию для регистрации
+    if ( $check_usr_name_password == 1 ) {
+        tools::task_8_reg_user( $user_name, $user_password );
+    } else {
+        print "Ошибка, введите корректное имя и пароль";
+    }
+} elsif ( $action eq "del" ) {
+    tools::task_10_del_user( $user_name, $user_password );
+} else {
+    print "Ошибка, неизвестное значение $action\n";
+}
