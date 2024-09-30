cde1104cfcc0b53964c9f0fd0b297b2ff634574c
Olga Nikonova <olga-nikonova-00@mail.ru>
2024-09-30 12:41:28 +0300

    add parser xml task

---
+++ tasks/parser.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+use XML::LibXML;
+
+my $conf_path = "tasks/files/";
+
+# Получение данных
+my $file_name = shift @ARGV;
+
+# Чтение файла
+my $document = XML::LibXML->new->parse_file($conf_path . $file_name);
+
+# Вывод результата
+print $document;
